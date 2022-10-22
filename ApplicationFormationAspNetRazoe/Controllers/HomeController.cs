8a2553d30f63d0225f176ab066befb2277ae52e5
labid <labid.imad@gmail.com>
2022-10-22 19:26:17 +0200

    mise à jour

--- ApplicationFormationAspNetRazoe/Controllers/HomeController.cs
+++ ApplicationFormationAspNetRazoe/Controllers/HomeController.cs
@@ -6,11 +6,9 @@ namespace ApplicationFormationAspNetRazoe.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
